11cc8d8929c4f971d2c2249c40fda741906f391f
Miranda <miranda.aldrin@gmail.com>
2019-10-09 21:22:03 +0200

    you cannow scan an nfc tag and get its NdefMessage

--- app/src/main/java/com/example/reach/NFCActivity.java
+++ app/src/main/java/com/example/reach/NFCActivity.java
@@ -1,5 +1,8 @@
 package com.example.reach;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,6 +11,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 
 import com.example.erikh.reach.R;
@@ -44,10 +49,26 @@ public class NFCActivity extends AppCompatActivity {
             Toast.makeText(this, "Turn on NFC to use this function",
                     Toast.LENGTH_SHORT).show();
         } else{
-            Toast.makeText(this, "NFC is enabled",
-                    Toast.LENGTH_SHORT).show();
+            onNewIntent(getIntent());
         }
 
+    }
+
+    protected void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+
+        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())){
+            Parcelable[] nfcMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if(nfcMessages != null){
+                NdefMessage[] messages = new NdefMessage[nfcMessages.length];
+                for(int i = 0; i<nfcMessages.length;i++){
+                    messages[i] = (NdefMessage) nfcMessages[i];
+                    System.out.println(messages);
+                }
+
+            }
+        }
     }
 
 }
