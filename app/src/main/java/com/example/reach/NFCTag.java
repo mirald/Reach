a33029e184deea2a4b48f1129d81e4b050a40323
Miranda <miranda.aldrin@gmail.com>
2019-10-09 23:03:18 +0200

    created an NFCTag class, with name and serial no

--- app/src/main/java/com/example/reach/NFCTag.java
+++ app/src/main/java/com/example/reach/NFCTag.java
@@ -1,4 +1,27 @@
 package com.example.reach;
 
 public class NFCTag {
+    private String serialNumber, name;
+
+
+    public NFCTag(String serialNumber, String name) {
+        this.serialNumber = serialNumber;
+        this.name = name;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
