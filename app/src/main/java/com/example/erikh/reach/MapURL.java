b4508d572bd86ce47d1b1e8772e822a4681d1f58
Miranda <miranda.aldrin@gmail.com>
2019-10-16 23:41:37 +0200

    markers change color when a tag is scanned

--- app/src/main/java/com/example/erikh/reach/MapURL.java
+++ app/src/main/java/com/example/erikh/reach/MapURL.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class MapURL {
     private String mapURL;
+    private CurrentRun currentRun;
+    private int width, height;
     private String API_key = BuildConfig.MapquestAPIKey;
     private String checkedTag = "|marker-FF8A65-lg";
     private String defaultColorOfMarker = "gray";
@@ -11,25 +13,41 @@ public class MapURL {
 
 
     public MapURL(CurrentRun cRun, int width, int height){
-        StringBuilder tempLocations = new StringBuilder();
+        this.currentRun = cRun;
+        this.width = width;
+        this.height = height;
+        String locations = getLocations(cRun);
+        this.mapURL = makeURL(locations, width, height);
+    }
 
+    private String getLocations(CurrentRun cRun){
+        StringBuilder tempLocations = new StringBuilder();
         for (Checkpoint key : cRun.getCurrentRun().keySet()){
             String location = Float.toString(key.getX())+","+Float.toString(key.getY());
-            if(cRun.getCurrentRun().keySet().size()>1){
-                location = location + "||";
-            }
             tempLocations.append(location);
             if(cRun.getStatus(key)){
                 tempLocations.append(checkedTag);
             }
+            if(cRun.getCurrentRun().keySet().size()>1){
+                tempLocations.append("||");
+            }
         }
 
-        this.mapURL = "https://www.mapquestapi.com/staticmap/v5/map?key="+API_key+"&locations" +
-                "="+ tempLocations.toString()+"&size="+ width + "," + height +
+        return tempLocations.toString();
+    }
+
+    private String makeURL(String loc, int w, int h){
+        return "https://www.mapquestapi.com/staticmap/v5/map?key="+API_key+"&locations" +
+                "="+ loc +"&size="+ w + "," + h +
                 "@2x&defaultMarker=marker-"+defaultColorOfMarker+"-lg&margin=200";
     }
 
     public String getMapURL() {
         return mapURL;
+    }
+
+    public void updateURL(CurrentRun currentRun){
+        String locations = getLocations(currentRun);
+        mapURL = makeURL(locations, this.width, this.height);
     }
 }
