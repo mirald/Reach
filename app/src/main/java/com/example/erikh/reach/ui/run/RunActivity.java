4695b80a649101f4d3c26278c2cdac274cd800c9
Miranda <miranda.aldrin@gmail.com>
2019-10-16 13:50:12 +0200

    loads map in right size

--- app/src/main/java/com/example/erikh/reach/ui/run/RunActivity.java
+++ app/src/main/java/com/example/erikh/reach/ui/run/RunActivity.java
@@ -4,6 +4,9 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.nfc.NfcManager;
 import android.nfc.Tag;
@@ -11,16 +14,21 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.util.DisplayMetrics;
 import android.util.Log;
 
+import com.bumptech.glide.request.RequestOptions;
 import com.example.erikh.reach.BuildConfig;
 import com.example.erikh.reach.GlideApp;
 import com.example.erikh.reach.R;
 import com.example.erikh.reach.CheckpointDatabase;
 
 import android.nfc.NfcAdapter;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -42,11 +50,14 @@ public class RunActivity extends AppCompatActivity {
     private TextView textView;
     private NfcAdapter nfcAdapter;
     private PendingIntent pendingIntent;
+    int width, height;
     String serialNumber;
     CheckpointDatabase checkpoints;
 
     ImageView mapImageView;
     ProgressBar progressBar;
+
+    String API_key;
 
 
 
@@ -76,41 +87,54 @@ public class RunActivity extends AppCompatActivity {
                     new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
         }
 
-        String API_key = BuildConfig.MapquestAPIKey;
-        Log.d(TAG, API_key);
-
-        int height = getScreenHeight();
-        Log.d(TAG, Integer.toString(height));
-        int width = getScreenWidth();
-        Log.d(TAG, Integer.toString(width));
-
-        String mapURL =
-                "https://www.mapquestapi.com/staticmap/v5/map?key="+API_key+"&locations" +
-                "=57.708765,11.936681||57.706472,11.935180||57.707962,11.940713||57.704813,11" +
-                        ".941643||57.708435,11.943359||57.713327,11.940594||57.714843,11" +
-                        ".932599||57.717777,11.943984&size="+width+ "," + height;
-
         mapImageView = (ImageView) findViewById(R.id.map_image);
 
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
-        progressBar.setVisibility(View.VISIBLE);
 
-        //TODO add error and placeholder images
-        GlideApp.with(context).load(mapURL).listener(new RequestListener<Drawable>() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                progressBar.setVisibility(View.GONE);
-                Log.d(TAG, "Glide load failed");
-                return false;
-            }
+        API_key = BuildConfig.MapquestAPIKey;
 
+
+        mapImageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
-            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                progressBar.setVisibility(View.GONE);
-                Log.d(TAG, "Glide resource ready");
-                return false;
+            public void onGlobalLayout() {
+                //Remove it here unless you want to get this callback for EVERY
+                //layout pass, which can get you into infinite loops if you ever
+                //modify the layout from within this method.
+                mapImageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                //Now you can get the width and height from content
+                height = mapImageView.getHeight();
+                width = mapImageView.getWidth();
+
+                String mapURL = "https://www.mapquestapi.com/staticmap/v5/map?key="+API_key+"&locations" +
+                        "=57.708765,11.936681||57.706472,11.935180||57.707962,11.940713||57" +
+                        ".704813,11" +
+                        ".941643||57.708435,11.943359||57.713327,11.940594||57.714843,11" +
+                        ".932599||57.717777,11.943984&size="+ width + "," + height +
+                        "@2x&defaultMarker=marker-gray-lg";
+
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                GlideApp.with(context).load(mapURL).placeholder(new ColorDrawable(ContextCompat.getColor(context, R.color.whiteBackground)))
+                        .error(new ColorDrawable(ContextCompat.getColor(context,
+                                R.color.whiteBackground))).listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        Log.d(TAG, "Glide load failed");
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        Log.d(TAG, "Glide resource ready");
+                        return false;
+                    }
+                }).into(mapImageView);
             }
-        }).into(mapImageView);
+        });
 
     }
 
@@ -179,13 +203,5 @@ public class RunActivity extends AppCompatActivity {
             tagSerialNumber.append(s).append(' ');
         }
         return tagSerialNumber.toString();
-    }
-
-    public static int getScreenWidth() {
-        return Resources.getSystem().getDisplayMetrics().widthPixels;
-    }
-
-    public static int getScreenHeight() {
-        return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 }
