0fa411a224c52ade0b823ec5f9b8dd303d3ea743
Johan Erlandsson <erlandd6@gmail.com>
2019-10-18 15:06:10 +0200

    Added function to get phones location + getclosestcheckpoint in listview

--- app/src/main/java/com/example/erikh/reach/ui/run/listItem.java
+++ app/src/main/java/com/example/erikh/reach/ui/run/listItem.java
@@ -1,12 +1,15 @@
 package com.example.erikh.reach.ui.run;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.example.erikh.reach.CheckpointDatabase;
+import com.example.erikh.reach.MainActivity;
 import com.example.erikh.reach.R;
 import com.example.erikh.reach.Run;
 
@@ -33,10 +36,17 @@ public class listItem extends ArrayAdapter<Run> {
         TextView estimatedTime = (TextView) convertView.findViewById(R.id.estimatedTimeId);
         TextView location = (TextView) convertView.findViewById(R.id.locationId);
         TextView checkpointNumber = (TextView) convertView.findViewById(R.id.checkpointsId);
+        TextView closestCheckpoint = (TextView) convertView.findViewById(R.id.closestId);
+
+
+
         name.setText(run.getName());
         estimatedTime.setText(run.getEstimateAsString());
         location.setText(run.getLocation());
         checkpointNumber.setText(run.getNumberOfCheckpoints() + "");
+        float[] tempArr = new float[]{MainActivity.xcord, MainActivity.ycord};
+        closestCheckpoint.setText(CheckpointDatabase.getCheckpointDatabase().getClosestCheckpoint(tempArr).getName());
+        Log.d("Hejhoj", "getView: ");
 
 
         return convertView;
