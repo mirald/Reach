94c88f447247f22ee050c03666fc95eb5ee4dca6
Miranda <miranda.aldrin@gmail.com>
2019-10-18 10:50:15 +0200

    takes user to NFC settings if not enabled

--- app/src/main/java/com/example/erikh/reach/NFCPermissionDialog.java
+++ app/src/main/java/com/example/erikh/reach/NFCPermissionDialog.java
@@ -10,20 +10,18 @@ import androidx.fragment.app.DialogFragment;
 public class NFCPermissionDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage(R.string.NFC_question)
                 .setPositiveButton(R.string.enable_nfc, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
+                        // add startactivity nfc
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
+                        dialog.cancel();
                     }
                 });
-        // Create the AlertDialog object and return it
         return builder.create();
     }
 }
